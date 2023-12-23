17d6d52e97bc07125f31eddbd052063549898111
mai-hu-mukesh-parmar <mukesh.parmar@itpl.io>
2023-12-24 03:46:16 +0530

    trying to solve deploy error

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 	env: {
-		API_ENDPOINT: "http://api.weatherapi.com/v1/",
+		API_ENDPOINT: "https://api.weatherapi.com/v1/",
 		API_KEY: "7d7cb67de1b74876a51161757232012",
 	},
 };
