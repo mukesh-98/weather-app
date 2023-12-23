3fa8097d0e2181306f47a46ee7014f1d3be8d8ff
mai-hu-mukesh-parmar <mukesh.parmar@itpl.io>
2023-12-23 20:17:24 +0530

    init enviroment api and keys

--- next.config.js
+++ next.config.js
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+	env: {
+		API_ENDPOINT: "http://api.weatherapi.com/v1/",
+		API_KEY: "7d7cb67de1b74876a51161757232012",
+	},
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
